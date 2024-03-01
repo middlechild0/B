2863b0b59c967a6067b9b510864734f944ecf1e9
middlechild0 <jimmymathu8@gmail.com>
2024-03-01 17:41:44 +0300

    CompanyRegForm

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,7 @@ router.get('/signupForm', (req,res) => res.render('signupform'));
 //SignupOption Page
 router.get('/signupOptions', (req,res) => res.render('signupOptions'));
 
+//CompanyRegForm Page
+router.get('/CompanyRegForm', (req,res) => res.render('CompanyRegForm'));
 
 module.exports = router;
