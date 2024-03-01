112ff412a2c2b7f15982e0a43cefd4fda7f4d6ae
middlechild0 <jimmymathu8@gmail.com>
2024-03-01 16:23:33 +0300

    new

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //Login Page
-router.get('/login', (req,res) => res.send('Login'));
+router.get('/login', (req,res) => res.send('loginform'));
 
 //Register Page
 router.get('/register', (req,res) => res.render('Register'));
