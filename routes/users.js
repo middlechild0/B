cf7c415c5a000c56b59f633acc397ff32fcd2735
middlechild0 <jimmymathu8@gmail.com>
2024-03-05 08:07:11 +0300

    Login Complete

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//LoginOptions
+router.get('/loginOptions', (req,res) => res.render('loginOptions'));
 
 //LoginForm
 router.get('/loginForm', (req,res) => res.render('loginform'));
@@ -13,5 +15,8 @@ router.get('/signupOptions', (req,res) => res.render('signupOptions'));
 
 //CompanyRegForm Page
 router.get('/CompanyRegForm', (req,res) => res.render('CompanyRegForm'));
+
+//CompanyAdminLogin Page
+router.get('/CompanyAdminLogin', (req,res) => res.render('CompanyAdminLogin'));
 
 module.exports = router;
